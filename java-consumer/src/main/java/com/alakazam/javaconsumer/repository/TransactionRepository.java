81bc582e6f918bf564b5702b83472c6bc26bdb22
Joaobezerrajr <149264193+Joaobezerrajr@users.noreply.github.com>
2024-02-04 10:23:00 -0300

    Fix imports

--- java-consumer/src/main/java/com/alakazam/javaconsumer/repository/TransactionRepository.java
+++ java-consumer/src/main/java/com/alakazam/javaconsumer/repository/TransactionRepository.java
@@ -1,6 +1,9 @@
 package com.alakazam.javaconsumer.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.alakazam.javaconsumer.model.Transaction;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
